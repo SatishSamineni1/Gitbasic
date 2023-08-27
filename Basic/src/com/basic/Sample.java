8f911fb8873ac3375194e16afec6a0a253a1fc02
Satish Samineni <Satish Samineni@SatishSAMINENI.mynetworksettings.com>
2023-08-27 09:44:43 -0400

    method1 added feature method

--- Basic/src/com/basic/Sample.java
+++ Basic/src/com/basic/Sample.java
@@ -4,4 +4,8 @@ public class Sample {
 	public static void main(String args[]) {
 		System.out.println("Hello world program");
 	}
+	
+	public void method1() {
+		System.out.println("meyhod 1 added here.");
+	}
 }
